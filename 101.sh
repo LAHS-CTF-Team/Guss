23a31db3616a27b9cb58296f3274be50f713471b
3301-Management <thomas.merl@studentlaschools.net>
2017-11-02 00:31:04 -0600

    Create 101.sh

---
+++ 101.sh
@@ -0,0 +1,5 @@
+apt-get update
+apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
+pip install --upgrade pip
+pip install --upgrade pwntools
+echo "Done. No logs saved. Safe to close window."
